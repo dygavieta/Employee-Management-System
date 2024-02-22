e06ef5755d4191aef31486679184cae4e5490203
Don Michael Gavieta <don.gavieta@98labs.com>
2024-02-22 10:57:06 +0800

    added test

--- src/test/java/com/ems/Employee/Management/System/EmployeeManagementSystemApplicationTests.java
+++ src/test/java/com/ems/Employee/Management/System/EmployeeManagementSystemApplicationTests.java
@@ -1,13 +1,40 @@
 package com.ems.Employee.Management.System;
 
+import com.ems.Employee.Management.System.entity.Employee;
+import com.ems.Employee.Management.System.service.employee.EmployeeService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigInteger;
 
 @SpringBootTest
 class EmployeeManagementSystemApplicationTests {
 
+	@Autowired
+	private EmployeeService employeeService;
+
+
 	@Test
-	void contextLoads() {
+	void whenSavingEmployee_thenOk(){
+		Employee employee = new Employee();
+		employee.setLastName("Gavieta");
+		employee.setFirstName("Don Michael");
+		employee.setMiddleName("Yap");
+		employee.setFullName("Don Michael Yap Gavieta");
+		Employee savedEmployee = employeeService.addEmployee(employee);
+ 		assertThat(savedEmployee).isNotNull();
+	}
+
+	@Test
+	void whenDeleteEmployee_thenOk(){
+		Employee employee = employeeService.findByFullName("Don Michael Yap Gavieta");
+		Employee deletedEmployee = employeeService.deleteById(employee.getEmployeeId());
+		assertThat(deletedEmployee).isNotNull();
 	}
 
 }
